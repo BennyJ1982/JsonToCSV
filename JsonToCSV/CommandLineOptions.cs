d1757800298ffd8abbef370a99c3df0c5ff9b7a2
Benjamin Janecke <benjamin_j@gmx.de>
2016-08-30 07:48:19 +0200

    Initial commit

---
+++ JsonToCSV/CommandLineOptions.cs
@@ -0,0 +1,31 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonToCSV
+{
+	class CommandLineOptions
+	{
+		[OptionList('h', Required = false, Separator = ',', HelpText = "Specifies the header names, comma-separated.")]
+		public IList<string> Headers { get; set; }
+
+		[OptionList('c', Required = true, Separator = ',', HelpText = "Specifies the columns to export, as JSON paths, comma-separated.")]
+		public IList<string> Columns { get; set; }
+
+		[Option('r', Required = false, DefaultValue = "value", HelpText = "Specifies the JSON path to the root element whose children to export.")]
+		public string Root { get; set; }
+
+		[Option('u', Required = true, HelpText = "Specifies the url to download the JSON from.")]
+		public string Url { get; set; }
+
+		[Option('d', Required = false, DefaultValue = '|', HelpText = "Specifies the delimiter used when writing the CSV data.")]
+		public char Delimiter { get; set; }
+
+		[HelpOption(HelpText = "Display this help screen.")]
+		public string GetUsage()
+		{
+			return HelpText.AutoBuild(this, (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+		}
+	}
+}
---
+++ JsonToCSV/Program.cs
@@ -0,0 +1,86 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace JsonToCSV
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var options = new CommandLineOptions();
+			if (!CommandLine.Parser.Default.ParseArguments(args, options))
+			{
+				return;
+			}
+
+			var data = DownloadData(options.Url).Result;
+			var records = ParseJson(data, options.Root);
+			ExportToCSV(options, records);
+
+			Console.ReadKey();
+		}
+
+		private static async Task<string> DownloadData(string uri)
+		{
+			var request = WebRequest.CreateHttp(uri);
+			var response = await request.GetResponseAsync();
+
+			using (var responseStream = response.GetResponseStream())
+			{
+				using (var reader = new StreamReader(responseStream))
+				{
+					return await reader.ReadToEndAsync();
+				}
+			}
+		}
+
+		private static JArray ParseJson(string json, string pathToRoot)
+		{
+			var jsonObject = JObject.Parse(json);
+			return (JArray)jsonObject.SelectToken(pathToRoot);
+		}
+
+		private static void ExportToCSV(CommandLineOptions options, JArray records)
+		{
+			if (options.Headers.Any())
+			{
+				WriteHeaders(options.Headers, options.Delimiter);
+			}
+
+			foreach (var record in records)
+			{
+				WriteRecord(record, options.Columns, options.Delimiter);
+			}
+		}
+
+		private static void WriteHeaders(IEnumerable<string> headers, char delimiter)
+		{
+			Console.WriteLine(headers.Aggregate((line, header) => line + delimiter + header));
+		}
+
+		private static void WriteRecord(JToken record, ICollection<string> columns, char delimiter)
+		{
+			int columnsWritten = 0;
+			foreach (var column in columns)
+			{
+				var columnToken = record.SelectToken(column);
+				if (columnToken != null)
+				{
+					Console.Write(columnToken.ToString());
+				}
+
+				if (++columnsWritten < columns.Count)
+				{
+					Console.Write(delimiter);
+				}
+			}
+
+			Console.WriteLine();
+		}
+	}
+}
